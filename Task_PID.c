bc27ee054e6ae280e6470a77eaf940f11e61e173
Brice Thrower <bricethrower@gmail.com>
2016-04-01 14:58:40 -0500

    Created file, added psuedocode

---
+++ Task_PID.c
@@ -0,0 +1,33 @@
+/*
+ * Task_PID.c
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: Brice Thrower
+ *
+ *      Description: PID module to adjust duty cycle for a heater to control temperature
+ *
+ *      Notes:	This module will continuously calculate an error value (e) as
+ *      	the difference between a desired temperature (Setpoint SP) and a
+ *      	current temperature (Process Variable PV). The module will attempt
+ *      	to minimize the error over time by adjusting the duty cycle (Manipulated
+ *      	Variable MV) using a weighted sum.
+ *
+ *      Inputs:
+ *
+ *      Output:
+ *
+ */
+
+/*
+ * Psuedo-Code Algorithm for PID Controller
+ *
+ * 	Read desired temperature (SP)
+ * 	Read current temperature (PV)
+ * 	Calculate error (e)
+ * 	Calculate output duty cycle (MV)
+ *
+ *
+ *
+ */
+
+ */
