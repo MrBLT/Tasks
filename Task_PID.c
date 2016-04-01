5a3c6b999cb8aa4e46178b047cea14d1e2b32e5c
bthrower <b689t065@ku.edu>
2016-04-01 18:26:30 -0500

    Added Psuedocode

--- Task_PID.c
+++ Task_PID.c
@@ -19,15 +19,42 @@
  */
 
 /*
- * Psuedo-Code Algorithm for PID Controller
- *
- * 	Read desired temperature (SP)
- * 	Read current temperature (PV)
- * 	Calculate error (e)
- * 	Calculate output duty cycle (MV)
- *
- *
- *
- */
+   Psuedo-Code Algorithm for PID Controller
+ 
+  	Read desired temperature (SP)
+  	Read current temperature (PV)
+   	
+        Calculate error (e), update a running sum for (I), update a rate of change of e, 
+   	
+	    e = SP - PV;
+	   
+	Update a running sum for use in (I)
 
+	    sum_e += e;
+
+	Update the rate of change of e for use in (D)
+	    
+	    de = e - e_prev;
+
+	Store e for later use
+
+	    e_prev = e;
+
+	Calculate output duty cycle (MV) by summing each component
+	    (P) Proportional Control: MV is set in proportion to the existing e
+	    
+	        P = P_Gain * e;
+
+	    (I) Integral Action: increase or decrease MV in small persistent steps that are 
+	                         proportional to the current error. Over time the steps accumulate
+			         and add up dependent on past errors
+			     
+		I = I_Gain * (integral or sum of past errors from 0 to t)
+
+	    (D) Derivative Action: consider the rate of change of e, increase or decrease MV 
+	                           depending on how fast the error is approaching zero
+				   
+		D = D_Gain * (derivative or rate of change of e)
+
+	    
  */
